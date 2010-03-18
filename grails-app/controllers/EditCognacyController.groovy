52db5c56d96cfe1c734bb399f13afedd47ec4052
SimonGreenhill <simon@simon.net.nz>
2010-03-18 17:27:23 +1300

    Revert "refactoring filter code" - oops. Didn't seem to work. Will try again later.
    This reverts commit 0864851d6eaf47716be1993467c7211203182622.

--- grails-app/controllers/EditCognacyController.groovy
+++ grails-app/controllers/EditCognacyController.groovy
@@ -8,10 +8,113 @@ class EditCognacyController {
         save:['POST']
     ]
     
-    def getFilterList = LexicalFeatureController.getFilterList
-    def doFilter = LexicalFeatureController.doFilter
-    def filter = LexicalFeatureController.filter
-    def parseFilter = LexicalFeatureController.parseFilter
+    
+    def getFilterList = {
+        return [
+            englishHeadword:[displayName:'English Headword'],
+            spanishHeadword:[displayName:'Spanish Headword'],
+            portugueseHeadword:[displayName:'Portuguese Headword'],
+            frenchHeadword:[displayName:'French Headword'],
+            latinHeadword:[displayName:'Latin Headword'],
+            caseStudyRegion:[
+                displayName:'Case Study Region',
+                type:'select',
+                values:CaseStudyRegion.list()
+            ],
+            semanticField:[
+                displayName:'Semantic Field',
+                type:'select',
+                values:SemanticField.list()
+            ],
+            category:[
+                displayName:'Category',
+                type:'select',
+                values:LexicalFeatureCategory.list()
+            ],
+            partOfSpeech:[
+                displayName:'Part of Speech',
+                type:'select',
+                values:PartOfSpeech.list()
+            ],
+            exportSet:[
+                displayName:'Export Set',
+                type:'select',
+                values:ExportSet.list()
+            ],
+
+        ]
+    }
+
+    def doFilter = { params, filters ->
+        def c = LexicalFeature.createCriteria()
+        def results = c.list(params) {
+            if(filters['englishHeadword']) {
+                ilike('englishHeadword', filters['englishHeadword'].replaceAll(/\*/, '%'))
+            }
+            if(filters['spanishHeadword']) {
+                ilike('spanishHeadword', filters['spanishHeadword'].replaceAll(/\*/, '%'))
+            }
+            if(filters['portugueseHeadword']) {
+                ilike('portugueseHeadword', filters['portugueseHeadword'].replaceAll(/\*/, '%'))
+            }
+            if(filters['frenchHeadword']) {
+                ilike('frenchHeadword', filters['frenchHeadword'].replaceAll(/\*/, '%'))
+            }
+            if(filters['latinHeadword']) {
+                ilike('latinHeadword', filters['latinHeadword'].replaceAll(/\*/, '%'))
+            }
+            if(filters['semanticField']) {
+                eq('semanticField.id', filters['semanticField'].toLong())
+            }
+            if(filters['caseStudyRegion']) {
+                eq('caseStudyRegion.id', filters['caseStudyRegion'].toLong())
+            }
+            if(filters['category']) {
+                eq('category.id', filters['category'].toLong())
+            }
+            if(filters['partOfSpeech']) {
+                eq('partOfSpeech.id', filters['partOfSpeech'].toLong())
+            }
+            if(filters['exportSet']) {
+                eq('exportSet.id', filters['exportSet'].toLong())
+            }
+        }
+        return results
+    }
+    
+    // This method takes params and concatenates them together into a filter string.
+    def filter = {
+        // Remove the submit button
+        def newParams = [:]
+        params.each { key, value ->
+            if(value && !(key in(['_submit', 'action', 'controller']))) {
+                newParams[key] = value
+            }
+        }
+        def filterString = WebUtils.toQueryString(newParams)
+        if(filterString.length() > 1) {
+            filterString = filterString[1..(filterString.length()-1)]
+            redirect (view: index, params: [filter:filterString])
+        } else {
+            redirect (view: index)
+        }
+    }
+
+    // This method takes a filter string and produces a list of params
+    def parseFilter = { filterString ->
+        def filters = [:]
+        if(filterString) {
+            filterString.split("&").each { pair ->
+                def parts = pair.split("=")
+                def key = parts[0].decodeURL()
+                if(parts.length > 1) {
+                    def value = parts[1].decodeURL()
+                    filters[key] = value
+                }
+            }
+        }
+        return filters
+    }
     
     def index = {
         
