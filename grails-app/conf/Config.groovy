a8ff20c4e2157b9f5fc60f3d46c284c05a64d5d4
SimonGreenhill <simon@simon.net.nz>
2010-04-23 11:47:54 +1200

    turn off logging of stacktraces

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -57,7 +57,11 @@ log4j = {
     //appenders {
     //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
     //}
-
+    // error stdout: "StackTrace"
+    appenders {
+        'null' name:'stacktrace'
+    }
+    
     error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
 	       'org.codehaus.groovy.grails.web.pages', //  GSP
 	       'org.codehaus.groovy.grails.web.sitemesh', //  layouts
