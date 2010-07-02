cd75dfb02910b8c15da166adefe4ba6395f27dd6
Ben Wing <ben@benwing.com>
2010-07-01 20:09:45 -0500

    Add some comments

--- grails-app/domain/LanguageProperty.groovy
+++ grails-app/domain/LanguageProperty.groovy
@@ -1,9 +1,15 @@
+// Abstract out common features of all language-related domain classes.
 abstract class LanguageProperty {
     Date createdAt
     Date updatedAt
     User createdBy
     User updatedBy
 
+    // If we specify `constraints' directly, these will end up as
+    // the first columns in table views.  We usually want them last,
+    // or sometimes in the middle.  To implement this, we put the
+    // constraints in a function and call the function from the
+    // `constraints' list in each class.
     static addDateConstraints = {
         createdAt(display: false, nullable: true)
         updatedAt(display: false, nullable: true)
--- grails-app/domain/NamedLanguageProperty.groovy
+++ grails-app/domain/NamedLanguageProperty.groovy
@@ -1,3 +1,5 @@
+// Abstract out features of language-related domain classes that have a
+// `name' field, used to return the displayed name of a class.
 abstract class NamedLanguageProperty extends LanguageProperty {
     String name
 
